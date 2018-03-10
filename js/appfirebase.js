169a3df0e9469af87ca3658c5125b3e626a84bef
Samahara <le.chat.au.chapeau@hotmail.com>
2018-03-09 19:31:41 -0600

    agregando carpeta firebase

---
+++ js/appfirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAdmzqFVtpohujwlCI0hqI7XwTeN5qmuvY",
+    authDomain: "twitterv2-6b786.firebaseapp.com",
+    databaseURL: "https://twitterv2-6b786.firebaseio.com",
+    projectId: "twitterv2-6b786",
+    storageBucket: "twitterv2-6b786.appspot.com",
+    messagingSenderId: "221643658475"
+  };
+  firebase.initializeApp(config);
