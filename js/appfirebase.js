a373047f3fe2396f9134e12c2d1a7c5f46dfeb7e
Samahara <le.chat.au.chapeau@hotmail.com>
2018-03-13 21:32:26 -0600

    firebase y autentificación con twitter

--- js/appfirebase.js
+++ js/appfirebase.js
@@ -8,3 +8,26 @@
     messagingSenderId: "221643658475"
   };
   firebase.initializeApp(config);
+
+  // Get a reference to the database service
+  var database = firebase.database();
+
+  var provider = new firebase.auth.TwitterAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+    // You can use these server side with your app's credentials to access the Twitter API.
+    var token = result.credential.accessToken;
+    var secret = result.credential.secret;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
